e6d7e74b812a6546891ce5561b3bb438c8d50cc1
vbhagi7766 <144253440+vbhagi7766@users.noreply.github.com>
2023-11-24 12:53:03 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,1 +1,13 @@
-aiml
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI='mongodb+srv://test:12345@cluster0.ty5srsi.mongodb.net/');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
